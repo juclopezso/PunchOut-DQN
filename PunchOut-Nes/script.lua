b8b7332a998898f1e6c64b8d284f23086b67a712
Juan Camilo Lopez <juancho@MacBook-Air-de-Juan.local>
2019-02-11 13:59:57 -0500

    lua rewards updated

--- PunchOut-Nes/script.lua
+++ PunchOut-Nes/script.lua
@@ -1,29 +1,41 @@
 
+previousOpp_health = data.oppHealth
 previousMac_health = data.macHealth
-function damage_done()
+prev_oko = data.oppKo
+prev_macKo = data.macKo
+
+prev_h10 = data.hearts10
+prev_h1 = data.hearts1
+
+function check_reward()
+  if data.hearts10 == 0 and data.hearts1 == 0 and data.macHealth > 0 then
+      return -0.3
+  end
+
+  if data.hearts10 > prev_h10 or data.hearts1 > prev_h1 then
+    prev_h10 = data.hearts10
+    prev_h1 = data.hearts1
+    return 12.0
+  end
+
+  if prev_oko < data.oppKo then
+    prev_oko = data.oppKo
+    return 60
+  end
+  if prev_macKo < data.macKo then
+    prev_macKo = data.macKo
+    return -60
+  end
   if previousMac_health > data.macHealth then
-    local delta = data.macHealth - previous_healthMac
+    local delta = previousMac_health - data.macHealth
     previousMac_health = data.macHealth
     return -delta
-  else
-    return 0
   end
-end
-
-previousOpp_health = data.oppHealth
-function damage_recieved()
   if previousOpp_health > data.oppHealth then
-    local delta = data.oppHealth - previousOpp_health
+    local delta = previousOpp_health - data.oppHealth
     previousOpp_health = data.oppHealth
     return delta
-  else
-    return 0
   end
-end
 
-function done_check()
-  if data.macKo > 4 then
-    return true
-  end
-  return false
+  return 0
 end
