f9e70d13a4894cf9ab990348b3000c131dc56604
Juan Camilo Lopez <juancho@MacBook-Air-de-Juan.local>
2019-02-14 11:23:04 -0500

    more penalty missing punches

--- PunchOut-Nes/script.lua
+++ PunchOut-Nes/script.lua
@@ -9,22 +9,22 @@ prev_h1 = data.hearts1
 
 function check_reward()
   if data.hearts10 == 0 and data.hearts1 == 0 and data.macHealth > 0 then
-      return -0.3
+      return -0.5
   end
 
   if data.hearts10 > prev_h10 or data.hearts1 > prev_h1 then
     prev_h10 = data.hearts10
     prev_h1 = data.hearts1
-    return 30.0
+    return 40.0
   end
 
   if prev_oko < data.oppKo then
     prev_oko = data.oppKo
-    return 60
+    return 80
   end
   if prev_macKo < data.macKo then
     prev_macKo = data.macKo
-    return -60
+    return -80
   end
   if previousMac_health > data.macHealth then
     local delta = previousMac_health - data.macHealth
@@ -40,7 +40,7 @@ function check_reward()
   if prev_h1 > data.hearts1 then
     local delta = prev_h1 - data.hearts1
     prev_h1 = data.hearts1
-    return -8.0
+    return -10.0
   end
 
   return 0
