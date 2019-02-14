b84b84ea848a89614965bc833979a0f0116d8e45
Juan Camilo Lopez <juancho@MacBook-Air-de-Juan.local>
2019-02-14 17:29:55 -0500

    back to past rewards

--- PunchOut-Nes/script.lua
+++ PunchOut-Nes/script.lua
@@ -9,22 +9,22 @@ prev_h1 = data.hearts1
 
 function check_reward()
   if data.hearts10 == 0 and data.hearts1 == 0 and data.macHealth < 1 then
-      return -0.5
+      return -0.3
   end
 
   if data.hearts10 > prev_h10 or data.hearts1 > prev_h1 then
     prev_h10 = data.hearts10
     prev_h1 = data.hearts1
-    return 40.0
+    return 20.0
   end
 
   if prev_oko < data.oppKo then
     prev_oko = data.oppKo
-    return 80
+    return 50
   end
   if prev_macKo < data.macKo then
     prev_macKo = data.macKo
-    return -80
+    return -50
   end
   if previousMac_health > data.macHealth then
     local delta = previousMac_health - data.macHealth
@@ -40,8 +40,9 @@ function check_reward()
   if prev_h1 > data.hearts1 then
     local delta = prev_h1 - data.hearts1
     prev_h1 = data.hearts1
-    return -10.0
+    return -2.0
   end
+
 
   return 0
 
